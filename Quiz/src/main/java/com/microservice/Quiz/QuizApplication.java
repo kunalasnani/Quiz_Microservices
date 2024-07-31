d10b7ed2f89c8c8cecfb24d5201549c4f80b3200
kunal <asnanik13@gmail.com>
2024-08-01 03:26:59 +0530

    Created service registry for managing Question and Quiz services using Eureka server

--- Quiz/src/main/java/com/microservice/Quiz/QuizApplication.java
+++ Quiz/src/main/java/com/microservice/Quiz/QuizApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class QuizApplication {
+public class  QuizApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
